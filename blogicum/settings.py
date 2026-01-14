25c97fb2ba4cb085c652895a54376ef4c07e1672
Ксения Самодурова <ksyu.samodurova.07@bk.ru>
2026-01-15 00:54:33 +0300

    Готовый проект Блогикум: исправлена структура проекта, manage.py перемещен в blogicum/

--- blogicum/settings.py
+++ blogicum/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'blogicum_internal.urls'
+ROOT_URLCONF = 'blogicum.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'blogicum_internal.wsgi.application'
+WSGI_APPLICATION = 'blogicum.wsgi.application'
 
 
 # Database
